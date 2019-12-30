11ca35c27456e3a60052c262d35910a840c1539a
martintc <33560895+martintc@users.noreply.github.com>
2019-12-30 15:38:17 -0800

    Update Tokens.java
    Define Tokens (except for the comma operator). Method for reading in a character and returning token value

--- Tokens.java
+++ Tokens.java
@@ -1,3 +1,37 @@
 public enum Tokens {
+  
+  // Tokens List
+  
+  MOVE_RIGHT, // Move pointer to the right
+  MOVE_LEFT, // Move the pointer to the left
+  INCREMENT, // Increment value at pointer
+  DECREMENT, // Decrement value at pointer
+  PRINT, // Print current character at pointer location
+  // Comma token to be implemented later
+  START_LOOP, // Start of a loop
+  END_LOOP,
+  ERROR; // End of a Loop
+  
+  // Takes a char token and returns the token value
+  public Tokens identifyToken (Char t) {
+   switch (t) :
+    case ">" :
+      return MOVE_RIGHT;
+    case "<" : 
+      return MOVE_LEFT;
+    case "+" :
+      return INCREMENT;
+    case "-" :
+      return DECREMENT;
+    case "." :
+      return PRINT;
+    // placeholder for comma operator token
+    case "[" :
+      return START_LOOP;
+    case "]" :
+      return END_LOOP;
+    default :
+      return ERROR;
+  }
 
 }
