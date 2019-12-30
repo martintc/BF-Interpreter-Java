b004c7a253ac2f1cce147e28b3776043f93659bb
martintc <33560895+martintc@users.noreply.github.com>
2019-12-30 15:42:03 -0800

    Create ArrayField.java

---
+++ ArrayField.java
@@ -0,0 +1,17 @@
+public class ArrayField {
+  
+  private int[] array;
+  private int pointer;
+  
+  public ArrayField () {
+    array = new int[30000]; // standard size for BrainF*ck; can be shortened for made larger
+    pointer = 0; // Where the pointer is pointing
+  }
+  
+  public void moveRight () {
+    pointer++;
+  }
+  public void moveLeft () {
+  
+  
+}
