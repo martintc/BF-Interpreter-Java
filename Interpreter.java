4970bc16baac2e252a084e0e777f0f630d485db4
martintc <33560895+martintc@users.noreply.github.com>
2019-12-31 08:41:18 -0800

    Update Interpreter.java

--- Interpreter.java
+++ Interpreter.java
@@ -1,3 +1,19 @@
-public class Interpreter {
+import java.util.Scanner;
 
+public class Interpreter {
+    
+  // instance variables and datastructures
+  private ArrayField a;
+  private Scanner read;
+  
+  public Interpreter () {
+    a = new ArrayField();
+  }
+  
+  public void interpretFromTerminal () {
+    read = new Scanner(System.in);
+    String currentToken = "";
+    while (read.hasNext()); {
+      
+  
 }
